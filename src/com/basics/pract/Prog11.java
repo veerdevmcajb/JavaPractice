3869981d3e186a1804229d82ba143c11a8c9cb23
veerdevmcajb <veerdevmca@gmail.com>
2026-02-28 19:43:11 +0530

    basics

---
+++ src/com/basics/pract/Prog11.java
@@ -0,0 +1,12 @@
+package com.basics.pract;
+
+public class Prog11 {
+    public static void main(String[] args) {
+        String a="Good"+"Morning";
+        if(a=="Good Morning"){
+            System.out.println("hi");
+        }else {
+            System.out.println("Bye");
+        }
+    }
+}
